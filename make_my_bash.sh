2542ae7b68e3820ea7bed9e17ddd63077a1d6690
Yan Hu <adrian.hu.chn@outlook.com>
2022-06-27 17:37:58 +0800

    Fix logging

--- make_my_bash.sh
+++ make_my_bash.sh
@@ -8,30 +8,32 @@ NO_COLOR='\033[0m'
 BASH_RC=".bashrc"
 BASH_PROFILE=".bash_profile"
 BASH_ALIASES=".bash_aliases"
+
+# Supported Backend: cp, rsync and scp
 COPY_BACKEND="/usr/bin/cp"
 
-#err() {
-#  echo -e "${RED}ERROR${NO_COLOR} : $(date +'%Y-%m-%dT%H:%M:%S%z')]: $1" >&2
-#}
-#
-#warn() {
-#  echo -e "${YELLOW}WARNING${NO_COLOR} - [$(date +'%Y-%m-%dT%H:%M:%S%z')]: $1"
-#}
+err() {
+  echo -e "${RED}ERROR${NO_COLOR} : [$(date +'%Y-%m-%dT%H:%M:%S%z')]: $1" >&2
+}
+
+warn() {
+  echo -e "${YELLOW}WARNING${NO_COLOR} - [$(date +'%Y-%m-%dT%H:%M:%S%z')]: $1"
+}
 
 info() {
   echo -e "${GREEN}INFO${NO_COLOR} - [$(date +'%Y-%m-%dT%H:%M:%S%z')]: $1"
 }
 
-copy_and_overwrite() {
+backup() {
+  tar -czvf $HOME/bash_dotfiles_bak.tar.gz $HOME/.bash*
+}
+
+overwrite() {
   cp_command="${COPY_BACKEND} -f $(pwd)/$1 $HOME"
-  info "$cp_command"
+  warn "$cp_command"
   $cp_command
 }
 
-backup() {
-  info "Backing up .bash_ files ..."
-  tar -czvf $HOME/bash_dotfiles_bak.tar.gz $HOME/.bash*
-}
 
 source_file() {
   info "source $HOME/$BASH_PROFILE"
@@ -39,11 +41,13 @@ source_file() {
 }
 
 main() {
+  info "Backing up bash dot files ..."
   backup
-  copy_and_overwrite $BASH_PROFILE
-  copy_and_overwrite $BASH_RC
-  copy_and_overwrite $BASH_ALIASES
-  source_file
+  warn "Start overwriting bash dot files ..."
+  overwrite $BASH_PROFILE
+  overwrite $BASH_RC
+  overwrite $BASH_ALIASES
+  info "Backup finished."
 }
 
 main
